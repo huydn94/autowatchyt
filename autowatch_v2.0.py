55e8cb5752afe4f119581677979ff536f4a4471a
Đặng Ngọc Huy <localhost4@gmail.com>
2023-07-06 14:12:51 +0700

    update v2.1

--- autowatch_v2.0.py
+++ autowatch_v2.0.py
@@ -1,95 +1,77 @@
-import subprocess
 import time
 from tkinter import *
 from tkinter import ttk
 import psutil
 import random
 import tkinter as tk
-from tkinter import messagebox
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from pytube import YouTube 
+from pytube import YouTube
+import threading
+from tkinter import messagebox
 #open new window
 
-
-
-def open_new_window(url):
-    #set path
-    chrome_path = r"C:\Program Files\Google\Chrome\Application\chrome.exe"  # Path to Chrome executable
-    guest_profile_path = r"C:\Users\HuyDang\AppData\Local\Google\Chrome\User Data\Guest Profile"  # Path to guest profile directory
-
-    # Set Chrome options
-    options = Options()
-    options.binary_location = chrome_path
-    options.add_argument("--guest")
-    options.add_argument("--user-data-dir={}".format(guest_profile_path))
-
-    # Create a new instance of the Chrome driver with the specified options
-    browser = webdriver.Chrome(options=options)
-    # Open a new window
-    browser.get(url)
-    #time.sleep(5)
-    button_play_selector="#movie_player > div.ytp-cued-thumbnail-overlay > button" 
-    #playButton = browser.find_element(By.CSS_SELECTOR, button_play_selector)
-    wait = WebDriverWait(browser, 10)
-    playButton = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, button_play_selector)))
-    playButton.click()
-    time.sleep(watch_time)
-
-
+time_count = 0
 #apply tham số
 def apply_info():
-    global numberofview, linklist, wait_time, lenlinklist, number_of_tab#, watch_time
-    #watch_time = int(e5.get())
+    global time_out, linklist, lenlinklist, number_of_tab
     number_of_tab = int(e4.get())
-    wait_time = int(e3.get())
-    numberofview = int(e1.get())
+    #time_out = int(e1.get())
     linklist_txt = str(e2.get())
     linklist = linklist_txt.split(",")
     lenlinklist = len(linklist)
     #linklist_output = [link.split() for link in linklist_output]
 
+#mở cửa sổ guest mới
+def open_new_window(url):
+    #set path
+    chrome_path = r"C:\Program Files\Google\Chrome\Application\chrome.exe"  # Path to Chrome executable
+    #guest_profile_path = r"C:\Users\HuyDang\AppData\Local\Google\Chrome\User Data\Guest Profile"  # Path to guest profile directory
+
+    # Set Chrome options
+    options = Options()
+    options.binary_location = chrome_path
+    options.add_argument("--guest")
+    #options.add_argument("--user-data-dir={}".format(guest_profile_path))
+
+    # Create a new instance of the Chrome driver with the specified options
+    browser = webdriver.Chrome(options=options)
+
+    # Open a new window
+    browser.get(url)
+
+    button_play_selector="#movie_player > div.ytp-cued-thumbnail-overlay > button"
+    #click nút play
+    wait = WebDriverWait(browser, 10)
+    playButton = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, button_play_selector)))
+    playButton = browser.find_element(By.CSS_SELECTOR,button_play_selector)
+    playButton.click()
+    watch_time = watch_time_cr(url)
+    print (watch_time)
+    global time_count
+    time_count= time_count + watch_time
+    print (time_count)
+    #messagebox.showinfo("Pop-up", "Thời gian đã chạy là "+str(time_count))
+    time.sleep(watch_time)
+    
 #open từng link
-def open_main():
-    global watch_time
-    j=0
-    for i in range (numberofview):
-        new_link_list = linklist
-        for x in range (number_of_tab):
-            random_link = random.choice(new_link_list) # lấy link ngẫu nhiên
-            new_link_list = [x for x in linklist if x != random_link] #tạo list các link còn lại
-            #lấy độ dài video
-            yt=YouTube(random_link) 
-            video_length = yt.length
-            watch_time = (random.randint(25,99))*video_length
-            #print (video_length)            
-            open_new_window(random_link)   
+def open_main(thread_id):
+    print(f"Thread {thread_id} started.")
+    # lấy link ngẫu nhiên
+    random_link = random.choice(linklist)
+    print(random_link)
+    open_new_window(random_link)
+    print(f"Thread {thread_id} completed.")
 
-            #time.sleep(20)
-        j=j+1 #tăng biến đếm
-        progress_bar['value']=(j*100//numberofview) #tăng giá trị cho progress bar
-        pVal = str(j*100//numberofview) + "%"   #tăng giá trị cho label progress bar
-        parent.update_idletasks()
-
-        if j < numberofview:
-            progress_label = Label(parent, text=pVal)
-            progress_label.place(x=250, y=250)
-            parent.update_idletasks()
-
-        if j == numberofview:
-            progress_label = Label(parent, text="DONE!")
-            progress_label.place(x=250, y=250)
-            parent.update_idletasks()   
-
-        #print("lan thu: ",j)
-        time.sleep(wait_time)
-        close_all_chrome_windows()
-        time.sleep(random.randint(25,100))
-
-#    progress_bar.stop()
+def watch_time_cr(url):
+    #Tính thời gian xem video 25-99% thời lượng
+    yt=YouTube(url)
+    video_length = yt.length
+    watch_time = (random.randint(25,99))*video_length//100
+    return watch_time
 #close all chrome windows đã bật
 def close_all_chrome_windows():
     chrome_processes = [proc for proc in psutil.process_iter() if proc.name() == "chrome.exe"]
@@ -99,46 +81,65 @@ def close_all_chrome_windows():
             proc.kill()
         except psutil.NoSuchProcess:
             pass
-    # for proc in psutil.process_iter(['pid', 'name']):
-    #     if proc.info['name'] == 'chrome.exe':
-    #         proc.kill()
 
+# def close_session_chrome(session_id):
+#     service_session = Service('path/to/chromedriver.exe')
+#     service_session.stop_service()
+#     options = webdriver.ChromeOptions()
+#     options.add_argument(f'--remote-debugging-port={session_id}')
+#     driver = webdriver.Chrome(service=service_session, options=options)
+#     driver.quit()
 
+def thread_open():
+
+    # Create an initial pool of threads
+    threads = []
+    for i in range (number_of_tab):
+        thread = threading.Thread(target=open_main, args=(i,))
+        thread.start()
+        threads.append(thread)
+
+    # Main thread keeps running to maintain the thread pool
+    while True:
+        # Check if any thread has finished, and join them
+        for thread in threads:
+            if not thread.is_alive():
+                thread.join()
+                threads.remove(thread)
+                print("Thread joined.")
+
+        # Create new threads to maintain the pool size
+        while len(threads) < number_of_tab:
+            thread = threading.Thread(target=open_main, args=(i,))
+            thread.start()
+            threads.append(thread)
+            print("New thread created.")
+
+        
 if __name__ == "__main__":
     parent = Tk()
-    parent.geometry('300x300')
+    parent.geometry('500x500')
 
     #Number of view
-    view = Label(parent, text = "Số view").place(x = 30, y = 50)
-    e1 = Entry(parent)
-    e1.place(x = 100, y = 50)
-    
+    # view = Label(parent, text = "Max time").place(x = 30, y = 50)
+    # e1 = Entry(parent)
+    # e1.place(x = 100, y = 50)
+
     #list of link
     link = Label(parent, text = "Link List").place(x = 30, y = 90)
     e2 = Entry(parent)
     e2.place(x = 100, y = 90)
-
-    #wait time
-    wait_time = Label(parent, text = "wait time").place(x = 30, y = 130)
-    e3 = Entry(parent)
-    e3.place(x = 100, y = 130)
     
-    number_of_tab = Label(parent, text = "tab/list").place(x = 30, y = 170)
+    number_of_tab = Label(parent, text = "số tab").place(x = 30, y = 130)
     e4 = Entry(parent)
-    e4.place(x = 100, y = 170)
+    e4.place(x = 100, y = 130)
 
-    watch_time = Label(parent, text = "watch time").place(x = 30, y = 210)
-    e5 = Entry(parent)
-    e5.place(x = 100, y = 210)
+    # def time_count_def():
+    #     messagebox.showinfo("Pop-up", "Thời gian đã chạy là "+str(time_count))
 
     apply_button = ttk.Button(parent, text='Apply', command=apply_info).grid(row = 4, column = 0)
-    run_button = ttk.Button(parent, text='run', command=open_main).grid(row=4, column=1)
+    run_button = ttk.Button(parent, text='run', command=thread_open).grid(row=4, column=1)
+    #check_time_run = ttk.Button(parent, text='check time', command = time_count_def).grid(row =4 , column = 2)
     #kill_button = ttk.Button(parent, text='close all', command=close_all_chrome_windows).grid(row=4, column=2)
-    loading_label = Label(parent, text="Progress")
-    loading_label.place(x = 30, y = 250)
-
-    # Create a progress bar
-    progress_bar = ttk.Progressbar(parent, mode="determinate", length=125)
-    progress_bar.place(x = 100, y = 250)
 
     parent.mainloop()
--- test.py
+++ test.py
@@ -1,6 +1,19 @@
-from pytube import YouTube 
+import threading
+import time
 
-video = "https://www.youtube.com/watch?v=XSQT9RBKoCs" 
-yt = YouTube(video)
-video_length = yt.length
-print ( video_length)
+def worker(thread_id):
+    print(f"Thread {thread_id} started.")
+    # Simulating some work
+    time.sleep(10)
+    print(f"Thread {thread_id} completed.")
+
+# Create and start the threads
+for i in range(3):
+    thread = threading.Thread(target=worker, args=(i,))
+    thread.start()
+
+print("All threads started.")
+
+# Main program continues while threads are running
+
+print("Main program completed. Exiting.")
